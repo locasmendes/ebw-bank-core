fb2b8541250ed218ff2e66cf3f543b20ab5a9b56
Felipe Ramos <felipebotelho18@gmail.com>
2022-04-18 14:33:26 -0300

    index robots

--- resources/views/site/layouts/master.blade.php
+++ resources/views/site/layouts/master.blade.php
@@ -24,9 +24,42 @@
         <meta property="twitter:description" content="A EBW Bank é uma fintech que oferece soluções de pagamento digital e físico, com a maquininha Passou Ganhou. Nossos serviços são voltados para o empreendedor que quer ganhar tempo e impulsionar as vendas.">
         <meta property="twitter:image" content="{{ asset('images/ebw-share.jpg')  }}">
 
+        {{-- @production
+            <script async src="https://www.googletagmanager.com/gtag/js?id=AW-928223794"></script>
+            <script>
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            
+            gtag('config', 'AW-928223794');
+                </script>
+                <!-- Event snippet for Cadastro EBW Bank conversion page -->
+            <script>
+            gtag('event', 'conversion', {'send_to': 'AW-928223794/lcmJCJiZ1P8BELKkzroD'});
+            </script>
+            
+                <!-- Facebook Pixel Code -->
+            <script>
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '206222104371563');
+            fbq('track', 'PageView');
+            </script>
+            <noscript><img height="1" width="1" style="display:none"
+            src="https://www.facebook.com/tr?id=206222104371563&ev=PageView&noscript=1"
+            /></noscript>
+            <!-- End Facebook Pixel Code -->
+        @endproduction --}}
+    
         
-        <meta name="robots" content="noindex,nofollow">
-        <meta name="googlebot" content="noindex,nofollow">
+        <meta name="robots" content="index,follow">
+        <meta name="googlebot" content="index,follow">
         <link rel="canonical" href="{{ url()->current() }}">
         <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
